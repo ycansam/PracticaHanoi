63c1c57203781de0ec2fb3fad7a3ef04ca442963
Yeray Candel <ycansam@epsg.upv.es>
2021-03-01 16:25:52 +0100

    Clase Pila

--- Torres_de_Hanoi/Pila.cs
+++ Torres_de_Hanoi/Pila.cs
@@ -9,34 +9,35 @@ namespace Torres_de_Hanoi
     class Pila
     {
         public int Size { get; set; }
-        /* TODO: Elegir tipo de Top
-        public int Top { get; set; }
-        public String Top { get; set; }        
-        */
-        /* TODO: Elegir tipo de Elementos
-        public Disco[] Elementos { get; set; }
+        public Disco Top { get =>this.Top; set => this.Top = value; }
         public List<Disco> Elementos { get; set; }
-        */
 
-        /* TODO: Implementar métodos */
         public Pila()
         {
-
+            this.Size = 0;
         }
 
         public void push(Disco d)
         {
-
+            if (!this.Elementos.Contains(d))
+            {
+                this.Elementos.Add(d);
+                this.Size++;
+                this.Top = Elementos.ElementAt(Elementos.Count());
+            }
         }
-
         public Disco pop()
         {
-            return null;
+            return this.Top;
         }                
 
         public bool isEmpty()
         {
-            return true;
+            if(this.Elementos.Count() != 0)
+            {
+                return true;
+            }
+            return false;
         }
 
     }
