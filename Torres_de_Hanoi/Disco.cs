683aaa9a1141c5ce950358eabda5aa593ae71968
Yeray Candel <ycansam@epsg.upv.es>
2021-03-01 16:06:36 +0100

    Clase Disco

---
+++ Torres_de_Hanoi/Disco.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Torres_de_Hanoi
+{
+    class Disco
+    {
+
+        private int valor = 0;
+
+        public Disco(int v)
+        {
+            this.valor = v;
+        }
+    }
+}
