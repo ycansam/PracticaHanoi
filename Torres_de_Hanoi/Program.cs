247710954b35cf7ae560bd921e0e69c83bfb928e
Yeray Candel <ycansam@epsg.upv.es>
2021-03-29 16:09:59 +0200

    Opcion de elegir Recursivo Iterativo Creada

--- Torres_de_Hanoi/Program.cs
+++ Torres_de_Hanoi/Program.cs
@@ -16,6 +16,8 @@ namespace Torres_de_Hanoi
             Pila aux = new Pila();
             Pila fin = new Pila();
             int number;
+
+            Console.Write("Introduce numero de discos"+Environment.NewLine);
             bool success = Int32.TryParse(Console.ReadLine(), out number);
             if (success)
             {
@@ -28,32 +30,60 @@ namespace Torres_de_Hanoi
                 }
                 else if(numDiscos < 0)
                 {
-                    Console.Write("No se puede obtener un resultado, introduzca un numero de discos valido.");
+                    Console.Write("No se puede obtener un resultado, introduzca un numero de discos valido." +Environment.NewLine);
                     Console.ReadKey();
                 }
                 else
                 {
-                    List<Disco> l_discos = new List<Disco>();
-                    
-                    for (int i = numDiscos; i > 0; i--)
+                    Console.Write("Seleccione: 1-Iteravo, 2-Recursivo" + Environment.NewLine);
+                    Int32.TryParse(Console.ReadLine(), out number);
+                    if(number == 1)
                     {
-                        ini.push(new Disco(i));
-                        l_discos.Add(new Disco(i));
+                        List<Disco> l_discos = new List<Disco>();
+
+                        for (int i = numDiscos; i > 0; i--)
+                        {
+                            ini.push(new Disco(i));
+                            l_discos.Add(new Disco(i));
+                        }
+
+                        Hanoi hanoiManual = new Hanoi();
+                        int numMan = hanoiManual.iterativo(numDiscos, ini, fin, aux);
+                        Console.WriteLine("El resultado es: " + numMan);
+                        int res = 1;
+                        for (int i = 0; i < numDiscos; i++)
+                        {
+                            res = 2 * res;
+                        }
+                        res = res - 1;
+
+                        Console.WriteLine("El resultado con operaciones es: " + res);
+                        Console.ReadKey();
+                    }
+                    else
+                    {
+                        List<Disco> l_discos = new List<Disco>();
+
+                        for (int i = numDiscos; i > 0; i--)
+                        {
+                            ini.push(new Disco(i));
+                            l_discos.Add(new Disco(i));
+                        }
+
+                        Hanoi hanoiManual = new Hanoi();
+                        int numMan = hanoiManual.recursivo(numDiscos, ini, fin, aux);
+                        Console.WriteLine("El resultado de recursivo es: " + numMan);
+                        int res = 1;
+                        for (int i = 0; i < numDiscos; i++)
+                        {
+                            res = 2 * res;
+                        }
+                        res = res - 1;
+
+                        Console.WriteLine("El resultado con operaciones es: " + res);
+                        Console.ReadKey();
                     }
 
-                    Hanoi hanoiManual = new Hanoi();
-                    int numMan = hanoiManual.iterativo(numDiscos, ini, fin, aux);
-                    Console.WriteLine("El resultado es: " + numMan);
-                    int res = 1;
-                    for(int i =0;i<numDiscos; i++)
-                    {
-                        res = 2 * res;
-                    }
-                    res = res - 1;
-
-                    Console.WriteLine("El resultado con operaciones es: " +res);
-
-                    Console.ReadKey();
                 }
             }
             else
